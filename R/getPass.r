55e2ba8a6e3aeb2a5ae22ea6639474c1b4db348b
joel23888 <joel23888@users.noreply.github.com>
2016-04-27 12:18:53 +0900

    Add auto mode to allow terminal method from anywhere

--- R/getPass.r
+++ R/getPass.r
@@ -61,7 +61,72 @@ getPass <- function(msg="PASSWORD: ", forcemask=FALSE)
   pw
 }
 
-
+#' Password Input
+#'
+#' Password reader.  Like R's \code{readline()} but the user-typed input
+#' text is not printed to the screen.
+#' 
+#' @details
+#' Masking (i.e., not displaying the literal typed text
+#' as input) is supported on most, but not all
+#' platforms.  It is supported in RStudio, provided you
+#' have a suitable version of the GUI.  It should also work in the
+#' terminal on any major OS.  Finally, it will work in any environment
+#' where the tcltk package is available.
+#' 
+#' In the terminal, the maximum length for input is 200 characters.
+#' Additionally, messages printed to the terminal (including the 
+#' "*" masking) are printed to stderr.
+#' 
+#' @param msg
+#' The message to enter into the R session before prompting
+#' for the masked input.  This can be any single string,
+#' including a "blank", namely \code{""}.
+#' @param forcemask
+#' Logical; should the function stop with an error if masking
+#' is not supported? If \code{FALSE}, the function will default
+#' to use \code{readline()} with a warning message that the
+#' input is not masked, and otherwise will stop with an error.
+#' 
+#' @return
+#' If input is provided, then that is returned. If the user cancels
+#' (e.g., cancel button on RStudio or ctrl+c in the terminal), then
+#' \code{NULL} is returned.
+#' 
+#' @examples
+#' \dontrun{
+#' # Basic usage
+#' getPass::getPass()
+#' 
+#' # Get password with a custom message
+#' getPass::getPass("Enter the password: ")
+#' }
+#'
+#' @export
+getPass2 <- function(msg="PASSWORD: ", forcemask=FALSE, auto=TRUE)
+{
+  if (!is.character(msg) || length(msg) != 1)
+    stop("argument 'msg' must be a single string")
+  if (!is.logical(forcemask) || length(forcemask) != 1 || is.na(forcemask))
+    stop("argument 'forcemask' must be one of 'TRUE' or 'FALSE'")
+  
+  if(auto) {
+    if (tolower(.Platform$GUI) == "rstudio")
+      pw <- readline_masked_rstudio(msg, forcemask)
+    else if (isaterm())
+      pw <- readline_masked_term(msg, showstars=TRUE)
+    else if (hastcltk())
+      pw <- readline_masked_tcltk(msg)
+    else if (!forcemask)
+      pw <- readline_nomask(msg)
+    else
+      stop("Masking is not supported on your platform!")
+  } else {
+    pw <- readline_masked_term(msg, showstars=TRUE)
+  }
+  
+  pw
+}
 
 readline_nomask <- function(msg, silent=FALSE)
 {
